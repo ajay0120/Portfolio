8d42dafff8ff0ab28c25e408858905d25aec182c
Potuganti Ajay Kumar <143871370+ajay0120@users.noreply.github.com>
2025-05-20 08:05:46 +0000

    changed initial details

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Potuganti Ajay Kumar",
+  title: "Full Stack Developer",
+  email: "potugantiajay0120@gmail.com",
+  gitHub: "https://github.com/ajay0120",
+  instagram: "https://www.instagram.com/p_ajay_2005/",
+  linkedIn: "https://www.linkedin.com/in/ajay-kumar-potuganti/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
